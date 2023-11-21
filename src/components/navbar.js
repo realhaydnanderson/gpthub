8680af26f45ebf61b1db702c6ee30fda1baf569e
realhaydnanderson <98076021+realhaydnanderson@users.noreply.github.com>
2023-11-20 23:34:54 -0800

    Added mobile sidebar.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,6 +4,8 @@ import supabase from '../supabase/supabaseClient';
 
 const Navbar = () => {
     const { user, setUser } = useAuth();
+    const [isMenuOpen, setIsMenuOpen] = useState(false); // State for mobile menu visibility
+
 
     const handleLogout = async () => {
         try {
@@ -16,12 +18,17 @@ const Navbar = () => {
         }
       };
 
+      const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen); // Toggle the state of the mobile menu
+    };
+
     return (
+        <>
             <header className="absolute inset-x-0 top-0 z-50">
                 <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
                     <div className="flex lg:flex-1">
                         <a href="/" className="-m-1.5 p-1.5">
-                            <span className="">GPTHub</span>
+                            GPTHub
                             {/* <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" /> */}
                         </a>
                     </div>
@@ -31,20 +38,50 @@ const Navbar = () => {
                     </div>
                     <div className="hidden lg:flex lg:gap-x-12">
                         <a href="/list" className="text-sm font-semibold leading-6 text-gray-900">Marketplace</a>
-                        <a href="/list" className="text-sm font-semibold leading-6 text-gray-900">Marketplace</a>
                     </div>
                     <div className="hidden lg:flex lg:flex-1 lg:gap-x-12 lg:justify-end">
                     {user ? <a href="/addgpt" className="text-sm font-semibold leading-6 text-gray-900">Add GPT</a> : null}
                         {user ? <a href="/account" className="text-sm font-semibold leading-6 text-gray-900">Account</a> : <a href="/login" className="text-sm font-semibold leading-6 text-gray-900">Log in →</a>}
                         {user ? <a href="#" onClick={handleLogout} className="text-sm font-semibold leading-6 text-gray-900">Logout</a> : null}
                     </div>
-                </nav>
 
-                {/* Mobile menu, shown/hidden based on state */}
-                <div className="lg:hidden">
-                    {/* Placeholder for actual mobile menu items */}
-                </div>
+                    {/* ... other navbar content ... */}
+                    <div className="flex lg:hidden">
+                    <button onClick={toggleMenu} className="p-2">
+                        {/* Placeholder for the hamburger icon */}
+                        <span className="sr-only">Menu</span>
+                        &#9776; {/* This is the Unicode character for a hamburger icon */}
+                    </button>
+                    </div>
+                </nav>
             </header>
+
+            {/* Overlay for darkening the screen */}
+            {isMenuOpen && (
+                <div className="fixed inset-0 bg-black bg-opacity-50 z-30" onClick={toggleMenu}></div>
+            )}
+
+            {/* Drawer-style mobile menu */}
+            <div id="drawer-navigation" className={`fixed top-0 left-0 z-50 h-screen p-4 overflow-y-auto transition-transform ${isMenuOpen ? "translate-x-0" : "-translate-x-full"} bg-white w-64 dark:bg-gray-800`} tabIndex="-1" aria-labelledby="drawer-navigation-label">
+                <h5 id="drawer-navigation-label" className="text-base font-semibold text-gray-500 uppercase dark:text-gray-400">Menu</h5>
+                <button onClick={toggleMenu} type="button" data-drawer-hide="drawer-navigation" aria-controls="drawer-navigation" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 absolute top-2.5 end-2.5 inline-flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white">
+                    {/* Close icon */}
+                </button>
+                <div className="py-4 overflow-y-auto">
+                    {/* Menu items */}
+                    <a href="/list" className="block text-sm font-semibold leading-6 text-gray-900">Marketplace</a>
+                    {user ? (
+                        <>
+                            <a href="/addgpt" className="block text-sm font-semibold leading-6 text-gray-900">Add GPT</a>
+                            <a href="/account" className="block text-sm font-semibold leading-6 text-gray-900">Account</a>
+                            <a href="#" onClick={handleLogout} className="block text-sm font-semibold leading-6 text-gray-900">Logout</a>
+                        </>
+                    ) : (
+                        <a href="/login" className="block text-sm font-semibold leading-6 text-gray-900">Log in →</a>
+                    )}
+                </div>
+            </div>
+        </>
     );
 };
 
